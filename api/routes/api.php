58fa2a64c4d1f10db2b2edfc49de968bc5509161
Jokay03J <magrojoan3@gmail.com>
2024-07-23 16:39:21 +0200

    Update routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,34 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClassroomController;
+use App\Http\Middleware\MustBeStudent;
+use App\Http\Middleware\MustBeTeacher;
 use Illuminate\Support\Facades\Route;
 
-Route::post("/registerTeacher", [AuthController::class, "registerTeacher"]);
-Route::post("/login", [AuthController::class, "login"]);
+Route::controller(AuthController::class)->group(function () {
+    Route::post("/registerTeacher", "registerTeacher");
+    Route::post("/registerStudent", "registerStudent");
+    Route::post("/login", "login");
+    Route::post("/studentCode", "createStudentCode")->middleware([
+        "auth:sanctum",
+        MustBeTeacher::class,
+    ]);
+    Route::post("/attachStudent", "attachStudent")->middleware([
+        "auth:sanctum",
+        MustBeStudent::class,
+    ]);
+});
+
+Route::controller(ClassroomController::class)
+    ->prefix("/classroom")
+    ->middleware(["auth:sanctum", MustBeTeacher::class])
+    ->group(function () {
+        Route::post("/", "create");
+        Route::post("/add", "addStudent");
+        Route::delete("/remove", "removeStudent");
+        Route::delete("/{classroom}/delete", "destroy")->whereUuid("classroom");
+    });
+Route::get("/classroom", [ClassroomController::class, "index"])->middleware(
+    "auth:sanctum"
+);
