fbaefa1452aeccdac67a864cb48719d3d6de7cb8
Jokay03J <magrojoan3@gmail.com>
2024-07-25 22:30:37 +0200

    Add classroom relationship

--- api/app/Models/Train.php
+++ api/app/Models/Train.php
@@ -5,9 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Train extends Model
 {
     use HasUuids, HasFactory;
-    protected $fillable = ["classroom_id", "content", "expected"]
+    protected $fillable = ["classroom_id", "content", "expected"];
+
+    public function classroom(): HasOne
+    {
+        // Get classroom
+        return $this->hasOne(Classroom::class, "id", "classroom_id");
+    }
 }
