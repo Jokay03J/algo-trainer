499abc413da08a2efb57fad79ef5791c860f230a
Jokay03J <magrojoan3@gmail.com>
2024-07-31 15:23:23 +0200

    update user atom

--- client/atoms/user.ts
+++ client/atoms/user.ts
@@ -1,13 +1,16 @@
 import { atomWithStorage } from "jotai/utils";
 
 export interface User {
+  id: string | null;
   firstName: string | null;
   lastName: string | null;
   email: string | null;
   token: string | null;
+  type: "teacher" | "student";
 }
 
 export const DEFAULT_USER_VALUE = <User>{
+  id: null,
   token: null,
   email: null,
   lastName: null,
