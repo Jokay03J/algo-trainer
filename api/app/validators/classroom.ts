1a0ba8bb406dbbf08c7b3c70825dc59030c8d315
jokay03J <magrojoan3@gmail.com>
2024-11-01 02:48:48 +0100

    Update classrooms validator

--- api/app/validators/classroom.ts
+++ api/app/validators/classroom.ts
@@ -2,12 +2,18 @@ import vine from '@vinejs/vine'
 
 export const createClassroomValidator = vine.compile(
   vine.object({
-    name: vine.string(),
+    name: vine.string().minLength(3),
   })
 )
 
 export const inviteStudentValidator = vine.compile(
   vine.object({
     email: vine.string(),
+  })
+)
+
+export const updateClassroomValidator = vine.compile(
+  vine.object({
+    name: vine.string().minLength(3),
   })
 )
